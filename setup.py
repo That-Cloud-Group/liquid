ed26bf3eca9b6520c86bead2aad4addc87b14462
Richard Julian <richard@rjulian.net>
2024-06-06 23:33:33 +0200

    Initial project structure and requirements

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+distutils/setuptools install script.
+"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="boto3",
+    version="0.0.1",
+    description="An SDK for Security Tooling",
+    long_description="An SDK focused on automation for security tooling.",
+    author="That Cloud Group",
+    url="https://github.com/That-Cloud-Group/liquid",
+    scripts=[],
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[],
+    license="Apache License 2.0",
+    python_requires=">= 3.8",
+    project_urls={
+        "Source": "https://github.com/That-Cloud-Group/liquid",
+    },
+)
