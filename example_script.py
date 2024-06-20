8a7d4446fcee577a724c8436d7bb4068a4e5892e
Jeremiah Owen <jeremiah.owen@kroger.com>
2024-06-20 09:41:17 -0700

    add saas_user auth

--- example_script.py
+++ example_script.py
@@ -1,9 +1,20 @@
 """Simple example script to test"""
 
 import liquid
+import os
+if os.getenv("AQUA_SAAS"):
+    aqua_client = liquid.client("aqua_cwp", {"auth_options": {
+        "auth_type": "saas",
+        "ssl_verify": True}})
+    scopes = aqua_client.list_application_scopes()
 
-aqua_client = liquid.client("aqua_cwp", {"auth_options": {"ssl_verify": False}})
-scopes = aqua_client.list_application_scopes()
+    for scope in scopes:
+        print(f"Found application scope with name {scope['name']}")
 
-for scope in scopes:
-    print(f"Found application scope with name {scope['name']}")
+if os.getenv("AQUA_JWT"):
+    aqua_client = liquid.client("aqua_cwp", {"auth_options": {"ssl_verify": False}})
+    scopes = aqua_client.list_application_scopes()
+
+    for scope in scopes:
+        print(f"Found application scope with name {scope['name']}")
+
--- liquid/aqua_authentication.py
+++ liquid/aqua_authentication.py
@@ -4,6 +4,13 @@ import os
 from dotenv import load_dotenv
 import requests
 import urllib3
+import hashlib
+import hmac
+import json
+import time
+import jwt
+from urllib.parse import urlparse
+
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -29,10 +36,13 @@ class AquaAuthentication:
         if self.auth_type == "jwt":
             self.authenticate_jwt()
         elif self.auth_type == "saas":
-            self.authenticate_saas()
+            self.authenticate_user_saas()
+        elif self.auth_type == "saas_api":
+            self.authenticate_api_key_saas()
 
     def authenticate_jwt(self):
         """Authenticates with an aqua server to get a JWT for future requests."""
+        print("Authenticating with JWT.")
         if not self.auth_credentials:
             self.auth_credentials = {
                 "user": os.environ.get("AQUA_USER"),
@@ -72,6 +82,98 @@ class AquaAuthentication:
         )
         return get_response.json()
 
-    def authenticate_saas(self):
+    def authenticate_user_saas(self):
         """Authenticates with SaaS endpoint using API tokens"""
         print("Authenticating with SaaS.")
+        if not self.auth_credentials:
+            self.auth_credentials = {
+                "user": os.environ.get("AQUA_USER"),
+                "password": os.environ.get("AQUA_PASS"),
+            }
+        if (
+            self.auth_credentials["user"]
+            and self.auth_credentials["password"]
+        ):
+            data = {
+                "email": self.auth_credentials["user"],
+                "password": self.auth_credentials["password"],
+            }
+            login_response = requests.post(
+                "https://api.cloudsploit.com/v2/signin",
+                verify=self.ssl_verify,
+                json=data,
+                timeout=30,
+                headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+            )
+            if login_response.status_code == 200:
+                self.token = login_response.json().get("data").get("token")
+                print("Token generated")
+                self.auth_url=self.__get_token_endpoint()
+            else:
+                print(login_response.text)
+    
+    def authenticate_api_key_saas(self, csp_roles: list = [], api_key: str = "", api_secret: str = "") -> dict:
+        """Authenticating to the Aqua API using api key and api secret instead of use-based auth
+
+        Args:
+            csp_roles (list, optional): The Aqua role(s) to be associated with the token. Defaults to [].
+            api_key (str, optional): API_KEY generated upon api key creation. Defaults to "".
+            api_secret (str, optional): API_SECRET generated upon api key creation. Defaults to "".
+        """
+        api_key = api_key or os.environ.get("AQUA_API_KEY")
+        api_secret = api_secret or os.environ.get("AQUA_API_SECRET")
+        csp_roles = csp_roles or os.environ.get("AQUA_CSP_ROLES", "").split(",")
+        if not api_key or not api_secret:
+            print("Error: AQUA_API_KEY or AQUA_API_SECRET not set")
+        if not csp_roles:
+            print("Error: AQUA_CSP_ROLES not set")
+        url = "https://api.cloudsploit.com/v2/tokens"
+        body = {
+            "validity":240,
+            "allowed_endpoints": [
+                "DELETE",
+                "GET",
+                "POST",
+                "PUT",
+            ],
+            "csp_roles": csp_roles
+        }
+        headers = self.__generate_api_header_security(api_key, api_secret, url, body)
+        login_response = requests.post(
+            url,
+            verify=self.ssl_verify,
+            headers=headers,
+            data=body,
+            timeout=30,
+        )
+        if login_response.status_code == 200:
+                self.token = login_response.json().get("data")
+        else:
+            print(login_response.text)
+
+    def __generate_api_header_security(self, api_key, api_secret, url, body):
+        body_str = json.dumps(body, separators=(',', ':'))
+        timestamp = str(int(time.time() * 1000))
+        path = urlparse(url).path
+        string = timestamp + "GET" + path+ body_str
+
+        secret_bytes = bytes(api_secret, "utf-8")
+        string_bytes = bytes(string, "utf-8")
+
+        sig = hmac.new(secret_bytes, msg=string_bytes, digestmod=hashlib.sha256).hexdigest()
+
+        headers = {
+            "accept": "application/json",
+            "x-api-key": api_key,
+            "x-signature": sig,
+            "x-timestamp": timestamp,
+            "content-type": "application/json",
+        }
+        
+        return headers
+    def __get_token_endpoint(self):
+        """Looks at JWT Token payload to find custom url for cloud cwp endpoint"""
+        proper_cwp = jwt.decode(self.token, options={"verify_signature": False})[
+            "csp_metadata"
+        ]["urls"]["ese_url"]
+        return f"https://{proper_cwp}"
