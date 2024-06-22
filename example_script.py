352d8afdd8208782d1b52b031b35eaefe354fe8a
Jeremiah Owen <jeremiah.owen@kroger.com>
2024-06-22 13:22:21 -0700

    add more to examples

--- example_script.py
+++ example_script.py
@@ -2,7 +2,8 @@
 
 import liquid
 import os
-if os.getenv("AQUA_SAAS"):
+if not os.getenv("AQUA_URL") and os.getenv("AQUA_USER"):
+    #Saas with user-pass auth
     aqua_client = liquid.client("aqua_cwp", {"auth_options": {
         "ssl_verify": True}})
     scopes = aqua_client.application_scopes.list_application_scopes()
@@ -10,7 +11,17 @@ if os.getenv("AQUA_SAAS"):
     for scope in scopes:
         print(f"Found application scope with name {scope['name']}")
 
-if os.getenv("AQUA_JWT"):
+elif not os.getenv("AQUA_URL") and os.getenv("AQUA_API_KEY"):
+    #Saas with user-pass auth
+    aqua_client = liquid.client("aqua_cwp", {"auth_options": {
+        "ssl_verify": True}})
+    scopes = aqua_client.application_scopes.list_application_scopes()
+
+    for scope in scopes:
+        print(f"Found application scope with name {scope['name']}")
+
+elif os.getenv("AQUA_URL") and os.getenv("AQUA_USER"):
+    # this is for local aqua installation
     aqua_client = liquid.client("aqua_cwp", {"auth_options": {"ssl_verify": False}})
     scopes = aqua_client.application_scopes.list_application_scopes()
 
