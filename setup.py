92eaec55711311ca6a1808db2897571e5cd55baa
Richard Julian <richard@rjulian.net>
2024-07-09 21:12:40 +0200

    Add in proper dependencies

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ from setuptools import find_packages, setup
 
 setup(
     name="liquid-sdk",
-    version="0.0.1",
+    version="0.0.2",
     description="An SDK for Security Tooling",
     long_description="An SDK focused on automation for security tooling.",
     author="That Cloud Group",
     url="https://github.com/That-Cloud-Group/liquid",
     scripts=[],
     packages=find_packages(exclude=["tests*"]),
-    install_requires=[],
+    install_requires=["requests", "PyJWT"],
     license="Apache License 2.0",
     python_requires=">= 3.8",
     project_urls={
